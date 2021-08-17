f2ab514ff8f16447b89c55eeb787673de55e1788
Emilse <emilsearias@gmail.com>
2021-08-17 09:53:16 -0300

    se agrego el archivo hola mundo

---
+++ js/holaMundo.js
@@ -0,0 +1,29 @@
+//Esto es un comentario simple o de una sola linea
+
+/* Esto es un comentario
+de multiples 
+lineas */
+
+// mostrar un mensaje en consola
+console.log('Hola comision 16i');
+
+// mostrar un msj en el html
+document.write("Este es mi primer msj");
+
+// mostrar un msj en una ventana emergente
+// alert('Hola mundo');
+
+// crear una variable (var - let - const)
+
+let comision = '16i';
+
+document.write('<br>Comision: '+comision)
+
+// modificar el contenido de una variable
+comision = 'i';
+
+document.write('<br>Comision: '+comision)
+
+const anio = '2021';
+document.write(55+anio)
+
