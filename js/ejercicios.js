5caffaf02c3726cf68d6f70afb48927aed72969c
Emilse <emilsearias@gmail.com>
2021-08-17 10:57:31 -0300

    se agrego el ejercicio 5

---
+++ js/ejercicios.js
@@ -0,0 +1,7 @@
+// parseInt('numero')
+let numero1 = parseInt(prompt('Ingre un primer numero'));
+let numero2 = parseInt(prompt('Ingrese un segundo numero'));
+
+document.write('El resultado es: ' + (numero1+ numero2))
+// console.log(numero1)
+// console.log(numero2)
