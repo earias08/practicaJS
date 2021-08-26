e14f70d472e59fefdddff82d5a691ef1b3537896
Emilse <emilsearias@gmail.com>
2021-08-26 11:35:49 -0300

    se agregaron funciones que retornan un valor y funciones con parametros a nuestro archivo funciones.js

--- js/funciones.js
+++ js/funciones.js
@@ -6,10 +6,32 @@ function holaMundo(){
 }
 
 //funciones con parametros
-function saludar(nombre,apellido){
+function saludar(nombre, apellido){
+    let edad;
     document.write('<br>Hola mi nombre es: '+nombre+' y mi apellido es '+apellido);
+}
+
+//funciones que retornan un valor
+function convertirDolaresPesos(dolares){
+    let pesos = dolares * 190;
+    console.log(pesos)
+    // siempre el return es la ultima linea de codigo
+    return pesos;
 }
 
 
 // ejecutar o invocar o llamar a una funcion
 holaMundo();
+// llamar a una funcion con parametros
+saludar('Esteban','Marquez');
+saludar('Juan','Alaniz');
+
+// let nombreRequerido = prompt('Ingrese un nombre');
+// let apellidoRequerido = prompt('Ingrese un apellido');
+// saludar(nombreRequerido,apellidoRequerido);
+convertirDolaresPesos(100)
+
+let precioIphone = convertirDolaresPesos(599)
+
+document.write('<br>Iphone SE $'+precioIphone);
+document.write('<br>Jordan $'+convertirDolaresPesos(200));
